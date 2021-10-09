ecf6cefe72d11f983d63dfdc18bb8af1d5171f2b
Christophe JULLIEN <84345980+Chris-pyz@users.noreply.github.com>
2021-10-09 21:04:11 +0200

    add javascript page home succed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import { animationMenu } from '../components/home';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  animationMenu();
+});
