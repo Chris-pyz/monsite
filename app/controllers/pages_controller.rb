4b50c71998fe1da52aeff86ef104b993fdf31892
Christophe JULLIEN <84345980+Chris-pyz@users.noreply.github.com>
2021-10-09 20:03:16 +0200

    create db, routes, controller pages and views's name

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+
+  def home; end
+
+  def pourquoiweb; end
+
+  def conditionsgenerales; end
+
+  def politiquedeconf; end
+
+  def mentionslegales; end
+
+  def contact; end
+
+  def tarifscreation; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'pourquoi-un-site-web', to: 'pages#pourquoiweb'
+  get 'conditions-generales', to: 'pages#conditionsgenerales'
+  get 'politiques-de-confidentialites', to: 'pages#politiquedeconf'
+  get 'mentions-legales', to: 'pages#mentionslegales'
+  get 'contact', to: 'pages#contact'
+  get 'tarifs-creation-site-web', to: 'pages#tarifscreation'
 end
