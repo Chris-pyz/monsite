dbc3e078543b94a063dd1aa459f56a56ffd12f20
Christophe JULLIEN <84345980+Chris-pyz@users.noreply.github.com>
2021-10-10 11:46:37 +0200

    reset all css and JS ans HTML

--- app/javascript/components/home.js
+++ app/javascript/components/home.js
@@ -1,10 +1,5 @@
 const animationMenu = () => {
-const hamburger_menu = document.querySelector(".hamburger-menu");
-const container = document.querySelector(".container");
 
-hamburger_menu.addEventListener("click", () => {
-  container.classList.toggle("active");
-})
-}
+
 
 export { animationMenu };
